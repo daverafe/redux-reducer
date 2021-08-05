f14e5e565882eb318cf41166e03d44b14fa97a30
David Rafe <david.rafe@gmail.com>
2021-08-05 15:04:28 -0400

    commit tree c95690d2da3ede6697829662e0104b0394f480b9

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+  
+    switch(action.type) {
+        case 'ADD_FRIEND':
+            return {friends: state.friends.concat(action.friend)}
+        case 'REMOVE_FRIEND':
+            const removeFriend = state.friends.find(friend => {
+                return friend.id === action.id
+            })
+            return {friends: state.friends.filter(friend => {
+                return friend != removeFriend 
+            })}
+        default:
+            return state 
+    }
 }
